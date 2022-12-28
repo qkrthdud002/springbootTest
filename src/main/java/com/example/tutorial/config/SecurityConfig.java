bd3d1360e06f3c45d2b7ba45b06a75f5ae407ed1
qkrthdud002 <qkrthdud002@gmail.com>
2022-12-28 10:10:38 +0900

    jwt-tutorial

--- src/main/java/com/example/tutorial/config/SecurityConfig.java
+++ src/main/java/com/example/tutorial/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.tutorial.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/example/tutorial/entity/Authority.java
+++ src/main/java/com/example/tutorial/entity/Authority.java
@@ -1,4 +1,23 @@
 package com.example.tutorial.entity;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "authority")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+
 }
